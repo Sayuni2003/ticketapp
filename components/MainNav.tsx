f84d4d50484aaa351644674178fb5830da86e02e
Sayuni2003 <sayuni3manjusri@gmail.com>
2025-12-12 14:25:47 +0530

    completed

---
+++ components/MainNav.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+import ToggleMode from "./toggleMode";
+import MainNavLinks from "./MainNavLinks";
+import { getServerSession } from "next-auth";
+import options from "@/app/api/auth/[...nextauth]/option";
+
+const MainNav = async () => {
+  const session = await getServerSession(options);
+
+  console.log(session);
+
+  return (
+    <div className="flex justify-between">
+      <MainNavLinks role={session?.user.role} />
+      <div className="flex items-center gap-2">
+        {session ? (
+          <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
+        ) : (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
+
+        <ToggleMode />
+      </div>
+    </div>
+  );
+};
+
+export default MainNav;
